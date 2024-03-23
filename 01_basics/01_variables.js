4ceafef1f4436fec70c014738f2c098af69aa802
DEEPAK KUMAR DAS <89087538+awsmdeep@users.noreply.github.com>
2024-03-23 18:43:29 +0000

    added variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+
+
+const accountId= 134324
+let accountEmail= "deepakdas538@gmail.com"
+var accountPassword="232323"
+accountCity="chennai"
+
+let accountState;
+
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Jharkhand"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
