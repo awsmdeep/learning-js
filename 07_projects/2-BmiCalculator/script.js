4e53c0adfbd1f21826de85dfe152ffd5a8b84bb0
awsmdeep <deepakdas538@gmail.com>
2024-04-11 14:43:58 +0530

    added bmi calculator

---
+++ 07_projects/2-BmiCalculator/script.js
@@ -0,0 +1,22 @@
+const form= document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    const height=parseInt(document.querySelector('#height').value);
+    const weight=parseInt(document.querySelector('#weight').value);
+    const result=(document.querySelector('#results'));
+
+    if(height=== '' || height<0 || isNaN(height)){
+        result.innerHTML=`Enter a valid height`;
+    }else  if(weight=== '' || weight<0 || isNaN(weight)){
+        result.innerHTML=`Enter a valid weight`;
+    }
+    else{
+        const bmi=((weight/(height*height)/10000)).toFixed(2);
+        result.innerHTML=`<span>${bmi}</span>`
+    }
+    
+
+
+})
