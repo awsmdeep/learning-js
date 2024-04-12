d5ea94255172c5789ece4804d5bc55f585cca3d3
awsmdeep <deepakdas538@gmail.com>
2024-04-12 21:31:58 +0530

    guess the number

---
+++ 07_projects/4-guessTheNumber/script.js
@@ -0,0 +1,8 @@
+const randomNumber=parseInt(Math.random()*100+1);
+
+const submit= document.querySelector('#subt')
+const userInput=document.querySelector('#guessField')
+const guessSlot=document.querySelector('.guesses')
+const remaining=document.querySelector('.lastResult')
+const lowOrhigh=document.querySelector('.lowOrHi')
+const starOver=document.querySelector('.resultParas')
